23a1e9ae143066e245e42dbacdaeb2dedcb4be91
desiatcodaniel <desiatcodaniel@gmail.com>
2021-02-25 11:36:52 -0800

    Finalize Prompt UI
    Finalized Prompt UI Exercise. Still not clear about the concept of promises/async await. Why does for of work instead of forEach or Map?

--- exercises/72 - Async Prompts/scripts.js
+++ exercises/72 - Async Prompts/scripts.js
@@ -32,20 +32,26 @@ function ask(options) {
       skipButton.textContent = 'Cancel';
       popup.firstChild.appendChild(skipButton);
       // TODO: listen for a click on the cancel button.
-      popup.addEventListener(
-        'submit',
-        function(e) {
-          e.preventDefault();
-          console.log('submitted');
-          resolve(e.target.input.value);
-          // Remove it from the DOM entirely
-          destroyPopup(popup);
+      skipButton.addEventListener(
+        'click',
+        function() {
+          resolve(null); // Explicitly tell the promise to resolve with nothing
         },
         { once: true }
-      ); // Only listen for the event once
+      );
     }
     // Listen for the submit event on the inputs
-
+    popup.addEventListener(
+      'submit',
+      function(e) {
+        e.preventDefault();
+        console.log('submitted');
+        resolve(e.target.input.value);
+        // Remove it from the DOM entirely
+        destroyPopup(popup);
+      },
+      { once: true }
+    ); // Only listen for the event once
     // When someone does submit it, resolve the data that was in the input box
 
     // Insert that popup into the DOM
@@ -54,3 +60,50 @@ function ask(options) {
     popup.classList.add('open');
   });
 }
+
+const askQuestion = async e => {
+  const button = e.currentTarget;
+  // Check if it has a cancel button
+  const cancel = 'cancel' in button.dataset;
+  console.log(cancel);
+  // Call ask function to get user input
+  const answer = await ask({
+    title: button.dataset.question,
+    cancel,
+  });
+
+  console.log(answer);
+};
+
+// Select all buttons that have a question
+const btns = document.querySelectorAll('[data-question]');
+
+btns.forEach(button => button.addEventListener('click', askQuestion));
+
+// Fire popup sequentially
+const questions = [
+  { title: 'What is your name?' },
+  { title: 'What is your age?', cancel: true },
+  { title: "What is your dog's name?" },
+];
+
+const asyncMap = async (array, callback) => {
+  // Make an array to store results
+  const results = [];
+
+  // Loop over our array
+  for (const item of array) {
+    results.push(await callback(item));
+  }
+  console.log(results);
+  return results;
+};
+
+// For of allows pausing of loop using await
+// const askMany = async () => {
+//   for (const question of questions) {
+//     const answer = await ask(question);
+//     console.log(answer);
+//   }
+// };
+asyncMap(questions, ask);
