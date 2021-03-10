d469239c78475f2d7e4e088a7fee04a7b3fb6d85
desiatcodaniel <desiatcodaniel@gmail.com>
2021-03-10 12:54:37 -0800

    Update Dad Jokes
    Almost done with dad jokes - Need to fix the button text

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -13,8 +13,40 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
-  const response = await fetch('https://icanhazdadjoke.com/');
-  console.log(response);
+  // Pass an accept header
+  // Header - Additional information that comes along into the fetch - like an argument in functions
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: `application/json`,
+    },
+  });
+
+  // Need to put await here since it's waiting for data from the fetch
+  const data = await response.json();
+  return data;
 }
 
-fetchJoke();
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random * arr.length - 1)];
+  console.log(item);
+  if (item === not) {
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+const handleClick = async e => {
+  // Select to toggle the loader animation
+  const loader = e.currentTarget.querySelector('.loader');
+  loader.classList.toggle('hidden');
+  // Need to put await since it needs the data before moving forward
+  const { joke } = await fetchJoke();
+  // Replace html content
+  jokeHolder.textContent = joke;
+  loader.classList.toggle('hidden');
+  // Replace content of the button without using duplicates
+  console.log(e.target);
+  e.target.textContent = randomItemFromArray(buttonText, e.target.textContent);
+};
+
+jokeBtn.addEventListener('click', handleClick);
