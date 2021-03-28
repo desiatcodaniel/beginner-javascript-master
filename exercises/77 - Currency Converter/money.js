c7547fa7d62787493a50c64c8b5b96907d06ed7f
desiatcodaniel <desiatcodaniel@gmail.com>
2021-03-27 23:55:26 -0700

    Start Currency Converter

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -32,3 +32,28 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+// Select all select tags
+const selects = document.querySelectorAll('select');
+
+// Create a function that will populate the options
+const generateOptions = options => {
+  // Create an array to save the values
+  const optionsArray = [];
+  // Loop over the objects using for of and object.entries and save only the value
+  for (const [key, value] of Object.entries(options)) {
+    const tag = `<option value="${key}">${key} - ${value}</option>`;
+    // Push value into the array
+    optionsArray.push(tag);
+  }
+  return optionsArray.join('');
+};
+
+// Generate all items
+const optionsHTML = generateOptions(currencies);
+console.log(optionsHTML);
+
+// Use the generated items to add to html
+selects.forEach(select => {
+  select.innerHTML = optionsHTML;
+});
