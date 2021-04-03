f513963f12c0275f0c76aa739c14a026b3b021ba
desiatcodaniel <desiatcodaniel@gmail.com>
2021-04-02 22:12:09 -0700

    origin

--- exercises/83 - Security/nasty.js
+++ exercises/83 - Security/nasty.js
@@ -4,10 +4,12 @@ const input = document.querySelector('[name="input"]');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('nav button');
 input.addEventListener('input', () => {
+  // First argument is the value to sanitize, second argument {} are module specific attributes to specify which tags and attributes to forbid
   const clean = sanitize(input.value, {
     FORBID_ATTR: ['width', 'height', 'style'],
     FORBID_TAGS: ['style'],
   });
+  // Find all newline and replace them with br tags
   output.innerHTML = clean.replace(/\n/g, '<br>');
 });
 
